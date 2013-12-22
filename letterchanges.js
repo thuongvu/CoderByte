a9f7f0daa0d6fcfbcc9e974e248ef118cde4c1bc
thuongvu <thuongvuho@gmail.com>
2013-12-22 12:29:59 -0800

    Letter changes
    Improved, but still not complete

--- letterchanges.js
+++ letterchanges.js
@@ -1,18 +1,22 @@
-var str = "hello there";
-var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var found = [];
-var counter = 0
+function LetterChange(str) {
+	var alphabet = "  abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var found = [];
+	var counter = 0;
+	var re = /[aeiou]/g;
 
-		for (var i = 0; i < alphabet.length; i++) {
-			if (str.indexOf(alphabet[i], 0) != -1 ) {
-			found.push(i + 1)
+			for (var iterate = 0; iterate < alphabet.length; iterate++) {
+				if (alphabet.indexOf(str[iterate], 0) != -1 ) {
+				found.push(alphabet[alphabet.indexOf(str[iterate], 0) + 1])
+				}
+
 			}
+			var joined = found.join("")
+			console.log(joined)
+			var newStr = joined.replace(re, function (x) {
+				return x.toUpperCase();
+			})
+			console.log(newStr)
+}
 
-		}
-		console.log(found)
 
-		for (var i = 0; i < found.length; i++) {
-			found[i]
-		} 
-
-// not solved yet, incomplete
+// not solved yet, incomplete, CLOSE, missing puncutiona marks, etc
