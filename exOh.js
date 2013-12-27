3ee3157053e5ccf5b00c9dd9c819a182cc77b137
thuongvu <thuongvuho@gmail.com>
2013-12-27 11:43:20 -0800

    Ex Oh

---
+++ exOh.js
@@ -0,0 +1,51 @@
+function ExOh(str) { 
+	var reX = /[x]?/g;
+	var reO = /[o]?/g;
+	str.match(reX);
+	str.match(reO);
+
+	if (str.match(reX).length != str.match(reO).length) {
+		return false;
+	} else {
+		return true;
+	}
+	  // code goes here  
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+ExOh(readline());                            
+// FAILS TESTS
+
+
+
+
+//
+function ExOh(str) { 
+var o = "o";
+var x = "x";
+var counterO = 0;
+var counterX = 0;
+
+for (var i = 0; i < str.length; i++) {
+	if (str[i] === o) {
+		counterO++
+	} else if (str[i] === x) {
+		counterX++
+	};
+}
+if (counterO != counterX) {
+	return false;
+} else if (counterO === counterX) {
+	return true;
+}
+	  // code goes here  
+         
+}
+
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+ExOh(readline());  
+
+// PASSES ALL TESTS     
