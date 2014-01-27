848a0ed5507dc5a258f8a93f1cea1ea817ae52a3
thuongvu <thuongvuho@gmail.com>
2014-01-27 00:50:45 -0800

    array addition, finally!

--- arrayAdditionI.js
+++ arrayAdditionI.js
@@ -1,26 +1,31 @@
-arr = [1,2,3,100];
-var arr = [3,5,-1,8,12]
-var sorted = arr.sort(function (a,b) {
-	return a-b
+var arr = [4, 6, 23, 10, 1, 3];
+arr.sort(function(a,b) {
+	return a-b;
 });
-var highestNumber = sorted.pop()
+var highest = arr.pop()
 var sum = 0;
+var possible = false;
+for (var i = 0; i < arr.length; i++) {
+	sum = arr[i];
 
-for (var i = 0; i < sorted.length; i++) {
-	sum+= sorted[i]
-	if (sum === highestNumber) {
-		console.log("TRUE")
+	for (var j = 0; j < arr.length; j++) {
+		if (i !== j) {
+			sum+= arr[j];
+			if (sum === highest) {
+				possible = true;
+			}
+		}
 	}
 
-// 3 + 5 + -1 + 8 
-
-}
-for (var j = 0; j < sorted.length; j++) {
-	sum - sorted[i]
-	if (sum === highestNumber) {
-		console.log("TRUE")
+	for (var k = 0; k < arr.length; k++) {
+		if (i !== k) {
+			sum -= arr[k];
+			if (sum === highest) {
+				possible = true;
+			}
+		}
 	}
+
+	sum = 0
 }
-
-// INCOMPLETE
-
+return possible;
