9bdf987a9485123e02d60cc282c90f6a186dc7a2
thuongvu <thuongvuho@gmail.com>
2014-01-27 15:18:00 -0800

    alphabet soup update

--- alphabetSoup.js
+++ alphabetSoup.js
@@ -1,5 +1,5 @@
 function AlphabetSoup(str) { 
   // code goes here  
   return str.split("").sort().join(""); 
-         
+  // sort() function doesn't require any params because sort starts off lexiconically, rather than numerically.  that's why for numbers, must include function a-b etc        
 }
