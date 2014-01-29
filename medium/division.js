d1550d1222520cad797a7bfad9735741b0604832
thuongvu <thuongvuho@gmail.com>
2014-01-28 17:00:13 -0800

    fixed division - from num1 to num2 in for

--- medium/division.js
+++ medium/division.js
@@ -11,7 +11,7 @@ for (var i = 0; i < num1; i++) {
 	}
 }
 for (var i = 0; i < num2; i++) {
-	if (num1 % i === 0) {
+	if (num2 % i === 0) {
 		num2Factors.push(i)
 	}
 }
@@ -23,5 +23,13 @@ for (var i = 0; i < num1Factors.length; i++) {
 		}
 	}
 }
-
 return common[common.length - 1]
+
+
+// 1. When the input was 12 and 16 your output was incorrect.
+// 2. When the input was 4 and 3 your output was incorrect.
+// 3. When the input was 8 and 2 your output was incorrect.
+// 4. When the input was 100 and 50 your output was incorrect.
+// 5. When the input was 14 and 28 your output was incorrect.
+// 6. When the input was 1 and 1 your output was incorrect.
+// 7. When the input was 15 and 60 your output was incorrect.
