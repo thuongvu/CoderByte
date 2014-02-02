e517c94ca7bfdb720793934412bdc604cfc768e7
thuongvu <thuongvuho@gmail.com>
2014-02-01 23:44:57 -0800

    alphabet soup functional, array addition incomplete

--- alphabetSoup.js
+++ alphabetSoup.js
@@ -3,3 +3,26 @@ function AlphabetSoup(str) {
   return str.split("").sort().join(""); 
   // sort() function doesn't require any params because sort starts off lexiconically, rather than numerically.  that's why for numbers, must include function a-b etc        
 }
+
+
+
+// ==============functional============================================================================//
+
+function split(str){
+	return str.split("")
+};
+
+function sort(str) {
+	return str.sort()
+}
+
+function join(str) {
+	return str.join("")
+}
+
+function splitSortJoin(str) {
+	var split = str.split("");
+	var sorted = split.sort();
+	return sorted.join("")
+}
+splitSortJoin("WHAAAT")
--- arrayAdditionI.js
+++ arrayAdditionI.js
@@ -51,3 +51,74 @@ return possible;
 // 13 + 10 = 23; BINGO
 // k
 // 23 - 1//, etc
+
+
+
+// ============= FUNCTIONAL THIS IS HARD ==============================================================================
+																		//--------------
+																		// UNCOMPLETE
+																		//----------------
+// sort array
+// pop highest one
+// iterate over array
+// reduce function. add/subtract/etc
+// check
+
+// function popHighest(arr) {
+// 	return arr.pop()
+// }
+
+// function forEach(arr, action) {
+// 	for (var i = 0; i < arr.length; i++) {
+// 		action(arr[i])
+// 	}
+// }
+
+// function add(a,b) {
+// 	console.log(a + b)
+// 	return parseInt(a) + parseInt(b);
+// }
+
+// function subtract(a,b) {
+// 	return parseInt(a) - parseInt(b);
+// }
+
+// function addSubtract(combine, check, arr, base, highest) {
+// 	forEach(arr, function(ele) {
+// 		base = ele; // base = current element iteration we are on
+// 		forEach(arr, function(ele) { // another foreach
+// 			if (ele !== base) { // if ele in here === base, then no go.
+// 				combine(ele, base) // combined everything
+// 				if (check(base, highest) === false)// check it {
+// 					forEach(arr, function(ele) {
+// 						if (ele !==base) {
+// 							combine(ele, base)
+// 						}
+// 					})
+// 				}
+// 			}
+// 		})
+// 	})
+// }
+
+// function checkIfPossible(number, highest) {
+// 	var possible = false;
+// 	if (number === highest) {
+// 		possible = true;
+// 		console.log("possible is true")
+// 	}
+// 	return possible;
+// }
+
+// function workTogether(arr) {
+// 	var sorted = arr.sort(function(a,b) {
+// 		return a - b;
+// 	});
+// 	var highest = popHighest(sorted);
+// 	if (addSubtract(add, checkIfPossible, arr, 0, highest) === false) {
+// 		return addSubtract(subtract, checkIfPossible, arr, 0, highest)
+// 	}
+// }
+// workTogether([4, 6, 23, 10, 1, 3])
+
+
