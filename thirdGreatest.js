e9d0c77303931d8bd4941e5a830ee6f9c1ede617
thuongvu <thuongvuho@gmail.com>
2014-02-03 14:43:31 -0800

    third greatest functional

--- thirdGreatest.js
+++ thirdGreatest.js
@@ -3,3 +3,18 @@ strArr.sort(function(a, b) {
 	return b.length - a.length
 })
 return strArr[2]
+
+
+// =================== FUNCTIONAL ====================================================
+
+function sortString(arr) {
+	arr.sort(function(a,b) {
+		return b.length - a.length;
+	})
+	return arr;
+}
+
+function returnThirdString(arr) {
+	return sortString(arr)[2]
+}
+returnThirdString(["hi", "world", "after", "all"])
