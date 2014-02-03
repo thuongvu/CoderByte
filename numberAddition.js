be8de5292b97fa0e9802420b30c3ef754c51a45c
thuongvu <thuongvuho@gmail.com>
2014-02-02 22:36:11 -0800

    numberAddition functional

--- numberAddition.js
+++ numberAddition.js
@@ -23,3 +23,36 @@ var added = numbers.reduce(function(a,b) {
 return added;
 // LOL
 //1. When the input was "no numbers sorry**" your output was incorrect.
+
+
+// FUNCTIONAL ===================================================================================
+function forEach(array, func) {
+	for (var i = 0; i < array.length; i++) {
+		func(array[i]);
+	}
+}
+
+function matchNumbers(str) {
+	return str.match(/\d+/g);
+}
+
+function reduce(combine, array) {
+	var base = 0;
+	forEach(array, function(ele) {
+		base = combine(parseInt(ele), base)
+	})
+	return base;
+}
+
+function add(a,b) {
+	return a + b;
+}
+
+function parseStringAndAdd(str) {
+	return reduce(add, matchNumbers(str));
+}
+
+parseStringAndAdd("75Number9")
+
+
+
