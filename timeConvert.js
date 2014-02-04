b7d9d20db1b7278df6e29643c34bfbf28fa9a850
thuongvu <thuongvuho@gmail.com>
2014-02-03 16:34:40 -0800

    timeConvert functional

--- timeConvert.js
+++ timeConvert.js
@@ -24,3 +24,27 @@ if (num < 60) {
 var mins = num % 60;
 var time = hours + ":" + mins;
 return time
+
+// ============================ FUNCTIONAL =========================================
+
+function convert(num) {
+	var time = {};
+	if (num < 60) {
+		time.hour = 0
+	} else {
+		time.hour = Math.floor(num / 60)
+	}
+	time.min = num % 60;
+	return time;
+}
+
+function timeToString(time) {
+	return time.hour + ":" + time.min;
+}
+
+function convertTime(num) {
+	return timeToString(convert(num));
+}
+
+convertTime(90)
+
