0cc5abdc2566aba27766f656c2834c8ef9ad04f4
thuongvu <thuongvuho@gmail.com>
2014-02-03 17:10:13 -0800

    vowelCount functional

--- vowelCount.js
+++ vowelCount.js
@@ -10,5 +10,42 @@ function countVowels(str) {
 			pos = str.indexOf(vowels[i], pos + 1)
 		}
 	}
+	console.log(counter)
 }
-return counter;
+// return counter;
+
+countVowels("hello")
+
+// ============== FUNCTIONAL ======================================
+function forEach(array, func) {
+	for (var i = 0; i < array.length; i++) {
+		func(array[i])
+	}
+}
+
+function counter(str) {
+	if (str.match(/[aeiouAEIOU]/)) {
+		var counter = 1;
+	} else {
+		var counter = 0;
+	}
+	return counter;
+}
+
+function filter(filterFunc, array) {
+	var counter = 0;
+	forEach(array, function(ele) {
+		counter += filterFunc(ele);
+	})
+	return counter;
+}
+
+function splitStr(str) {
+	return str.split("")
+}
+
+function countVowels(str) {
+	return filter(counter, splitStr(str));
+}
+
+countVowels("hello")
