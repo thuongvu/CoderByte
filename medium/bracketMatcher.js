841c581bbe8fa28ef2f4db3a5151a03e7d0e2858
thuongvu <thuongvuho@gmail.com>
2014-02-04 16:05:21 -0800

    bracketMatcher

---
+++ medium/bracketMatcher.js
@@ -0,0 +1,22 @@
+function BracketMatcher(str) { 
+	var left = 0;
+	var right = 0;
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] === '(') {
+			left++
+		} else if (str[i] === ')') {
+			right++;
+		}
+	}
+
+	if (left === right) {
+		return 1
+	} else {
+		return 0
+	}
+         
+}
+
+BracketMatcher("(c(oder)) b(yte)")
+
+// 1. When the input was "the color re(d))()(()" your output was incorrect.
