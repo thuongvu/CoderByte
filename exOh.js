a2be2daaea74318760f3cd24103bd3fd4dacc571
thuongvu <thuongvuho@gmail.com>
2014-02-06 03:08:39 -0800

    exOh again + functional

--- exOh.js
+++ exOh.js
@@ -130,3 +130,114 @@ start("xooxxo")
 
 
 
+
+
+// ATTEMPT 3?
+// loop across string
+// count x's
+// count o's
+// if amount of x and o ===, return true, else false
+
+function exOh(str) {
+	var xCount = 0;
+	var oCount = 0;
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] === 'x') {
+			xCount++;
+		} else if (str[i] === 'o') {
+			oCount++;
+		}
+	}
+
+	if (xCount === oCount) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+exOh("xoxoxo")
+
+
+// functional
+
+function forEach(array, func) {
+	for (var i = 0; i < array.length; i++) {
+		func(array[i]);
+	}
+}
+
+function count(array, countFunc) {
+	var oCount = 0;
+	var xCount = 0;
+	forEach(array, function(ele) {
+		if (countFunc(ele) === 'x') {
+			xCount++;
+		} else {
+			oCount++;
+		}
+	})
+	if (oCount === xCount) {
+		return true
+	} else{
+		return false;
+	}
+}
+
+function xOrO(ele) {
+	if (ele === 'o') {
+		return "o";
+	} else {
+		return "x";
+	}
+}
+
+function compare(str) {
+	return count(str, xOrO);
+}
+
+compare("xoxo")
+
+
+// functional 3
+
+function forEach(array, func) {
+	for (var i = 0; i < array.length; i++) {
+		func(array[i]);
+	}
+}
+
+function count(array, countFunc) {
+	var countObj = {};
+	forEach(array, function(ele) {
+		countFunc(ele, countObj);
+	})
+	return countObj;
+}
+
+function xOrO(ele, countObj) {
+	if (countObj[ele] == null) {
+		countObj[ele] = 0;
+	} 
+	countObj[ele]++;
+}
+
+function compareObj(countObj) {
+	var propArr = [];
+	for (prop in countObj) {
+		propArr.push(prop);
+	}
+	for (var i = 1; i < propArr.length; i++) {
+		if (!(countObj[propArr[i]] === countObj[propArr[i - 1]])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function compare(str) {
+	return compareObj(count(str, xOrO));
+}
+
+compare("xoxox")
+
