2386b548d9920a8ddb452e91c7cad0d8650063c9
thuongvu <thuongvuho@gmail.com>
2014-02-07 01:53:50 -0800

    multipleBrackets

---
+++ medium/multipleBrackets.js
@@ -0,0 +1,32 @@
+function MultipleBrackets(str) { 
+	var parenCheck = 0;
+	var bracketCheck = 0;
+	var paren = 0;
+	var bracket = 0;
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] === '[') {
+			bracketCheck++
+			bracket++
+		} 
+		if (str[i] === ']') {
+			bracketCheck--
+		} 
+		if (str[i] === '(') {
+			parenCheck++
+			paren++
+		} 
+		if (str[i] === ')') {
+			parenCheck--
+		} 
+	}
+	if ((paren === 0) && (bracket === 0)) {
+		return 1;
+	} else if ((parenCheck === 0) && (bracketCheck === 0)) {
+		return 1 + " " + (paren + bracket)
+	} else if (!((parenCheck === 0) && (bracketCheck === 0))) {
+		return 0;
+	}
+         
+}
+
+MultipleBrackets("hello");
