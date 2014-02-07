2c927c3fd90efd795b4b09210803b6f587910c0f
thuongvu <thuongvuho@gmail.com>
2014-02-07 11:26:58 -0800

    lettercount++ & functional

--- medium/letterCount.js
+++ medium/letterCount.js
@@ -37,3 +37,84 @@ LetterCount("Hello apple pie")
 // 2. When the input was "I lied before" your output was incorrect.
 // 3. When the input was "coderbyte" your output was incorrect.
 // 4. When the input was "red none yellow" your output was incorrect.
+
+
+// 2nd attempt
+function LetterCount(str) {
+	var words = str.split(" ");
+	var wordObj = {};
+	var highestFrequency = 0;
+	var highestWord;
+	for (var i = 0; i < words.length; i++) {
+		var word = words[i].split("");
+		for (var j = 0; j < word.length; j++) {
+			if (word[j] === word[j - 1]) {
+				if (wordObj[word] == null) {
+					wordObj[word] = {};
+					wordObj[word][word[j]] = 0;
+				}
+				wordObj[word][word[j]]++;
+				if (wordObj[word][word[j]] > highestFrequency) {
+					highestFrequency = wordObj[word][word[j]];
+					highestWord = word.join("")
+				}
+			}
+		}
+	}
+	return highestWord;
+}
+
+LetterCount("Hello apple pie")
+
+// functional
+
+function forEach(array, func) {
+	for (var i = 0; i < array.length; i++) {
+		func(array[i]);
+	}
+}
+
+function reduce(array, combine) {
+	var wordObj = {};
+	forEach(array, function(word) {
+		combine(word, wordObj)
+	})
+	return wordObj
+}
+
+function countLetters(word, wordObj) {
+	var letters = word.split("");
+	forEach(letters, function (letter) {
+		if (letter.match(/[a-zA-Z]/)) {
+			if (wordObj[word] == null) {
+				wordObj[word] = {};
+			}
+			if (wordObj[word][letter] == null) {
+				wordObj[word][letter] = 0;
+			}
+			wordObj[word][letter]++;
+		}
+	})
+}
+
+function compareObjs(wordObj) {
+	var highestFrequency = 0;
+	var highestWord;
+	for (words in wordObj) {
+		for (wordLength in words) {
+			if (wordLength > highestFrequency) {
+				highestFrequency = wordLength;
+				highestWord = words
+			}
+		} 
+	}
+	return highestWord;
+}
+
+function LetterCount(str) {
+	var words = str.split(" ")
+	return compareObjs(reduce(words, countLetters));
+}
+
+LetterCount("Hello apple pie")
+
